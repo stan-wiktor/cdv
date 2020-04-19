8e3b25fc1b21a674251237e9d0f664adc0681d58
stan-wiktor <wiks5@o2.pl>
2020-04-19 11:48:17 +0200

    1

---
+++ programowanie_systemowe/copy.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define BUF_SIZE = 1024
+
+int main(int argc, char *argv[])
+{
+    int input_fd, output_fd;
+    ssize_t ret_in, ret_out;
+    char buffer[BUF_SIZE];
+
+
+    if(argc !=3 || strcmp(argv[1], "--help") == 0)
+    {
+        printf("Usage: %s file_origin file_dest\n", argv[0]);
+        return 1;
+    }
+
+    input_fd = open(argv[1], O_RDONLY);
+    if(input_fd == -1)
+    {
+        perror("open");
+        return 2;
+    }
+
+    output_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(output_fd == -1)
+    {
+        perror("create");
+        return 3;
+    }
+
+    while((ret_in = read(input_fd, &buffer, BUF_SIZE)) > 0)
+    {
+        ret_out = write(output_fd, &buffer, ret_in);
+        if(ret_out != ret_in)
+        {
+            perror("copy");
+            return 5;
+        }
+    }
+
+    close(input_fd);
+    close(output_fd);
+    return 0;
+}
